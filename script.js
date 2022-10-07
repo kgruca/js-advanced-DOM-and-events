8f811364154249a20b719ef2e4d551813033065e
Krzysztof Gruca <kgruca21@gmail.com>
2022-10-06 23:44:53 -0700

    selecting elements

--- script.js
+++ script.js
@@ -38,3 +38,18 @@ document.addEventListener('keydown', function (e) {
 
 // NEW SECTION
 // Selecting, Creating, and Deleting Elements
+
+// can select entire document of any webpage:
+console.log(document.documentElement);
+// logs the code structure of the html for the document
+// can't just use "document" b/c it's not a real DOM element
+// can also select head and body:
+console.log(document.head);
+console.log(document.body);
+
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+// logs a nodelist of 4 elems
+
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
