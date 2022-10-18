50a0ddd68bc51d7e3e3eafe7c2fac1df7e0a91f7
Krzysztof Gruca <kgruca21@gmail.com>
2022-10-18 00:37:48 -0700

    implemented various methods of smooth scrolling

--- script.js
+++ script.js
@@ -30,12 +30,48 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+// implement smooth scrolling
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function(e) {
+    const s1coords = section1.getBoundingClientRect();
+    console.log(s1coords);
+
+    console.log(e.target.getBoundingClientRect());
+
+    console.log('Current Scroll (x/y)', window.pageXOffset, window.pageYOffset);
+
+    console.log(
+        'height/width viewport',
+        document.documentElement.clientHeight,
+        document.documentElement.clientWidth
+    );
+
+    // Scrolling
+    // window.scrollTo(
+    //     s1coords.left + window.pageXOffset, 
+    //     s1coords.top + window.pageYOffset
+    // );
+
+    // // Can make the scrolling more smooth by:  
+    // window.scrollTo({
+    //     left: s1coords.left + window.pageXOffset, 
+    //     top: s1coords.top + window.pageYOffset,
+    //     behavior: 'smooth'
+    // });
+
+    // an even more modern way:
+    section1.scrollIntoView({behavior: 'smooth'});
+});
+
 
 ///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
 // LECTURES
 
 
+/*
 // NEW SECTION
 // Selecting, Creating, and Deleting Elements
 
@@ -106,3 +142,4 @@ console.log(getComputedStyle(message).color);
 
 // to change style, ex.
 message.style.height = getComputedStyle(message).height + 40 + 'px';
+*/
