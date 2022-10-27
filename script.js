65f80ba2d40ee5c945dfee49d20104890263e051
Krzysztof Gruca <kgruca21@gmail.com>
2022-10-26 22:26:21 -0700

    opacity returns to normal for links in mouseout event listener

--- script.js
+++ script.js
@@ -131,14 +131,21 @@ nav.addEventListener('mouseover', function(e) {
       const logo = link.closest('.nav').querySelector('img');
 
       siblings.forEach(el => {
-        if(el !== link) el.style.opacity = 0.5;
+          if(el !== link) el.style.opacity = 0.5;
       });
       logo.style.opacity = 0.5;
     }
 });
 
 nav.addEventListener('mouseout', function(e){
-    
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+        if(el !== link) el.style.opacity = 1;
+    });
+    logo.style.opacity = 1;
 });
 
 
