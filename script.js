a15e786d1528437ab2cf2933258dcc6bc6bef7d8
Krzysztof Gruca <kgruca21@gmail.com>
2022-10-26 22:34:13 -0700

    changed handleHover to pass in only the target

--- script.js
+++ script.js
@@ -123,22 +123,21 @@ tabsContainer.addEventListener('click', function(e) {
 });
 
 
-const handleHover = function(e, opacity) {
+const handleHover = function(e) {
     if(e.target.classList.contains('nav__link')) {
         const link = e.target;
         const siblings = link.closest('.nav').querySelectorAll('.nav__link');
         const logo = link.closest('.nav').querySelector('img');
 
         siblings.forEach(el => {
-            if(el !== link) el.style.opacity = opacity;
+            if(el !== link) el.style.opacity = this;
         });
-        logo.style.opacity = opacity;
+        logo.style.opacity = this;
     }
 };
 
-// Menu fade animation
+// Passing argument into handler
 nav.addEventListener('mouseover', handleHover.bind(0.5));
-
 nav.addEventListener('mouseout', handleHover.bind(1));
 
 
