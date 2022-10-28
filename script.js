9af954ce82b30ca0599db5cffb16d6af1407833e
Krzysztof Gruca <kgruca21@gmail.com>
2022-10-27 19:31:25 -0700

    code implemented for revealing elements upon scroll

--- script.js
+++ script.js
@@ -187,6 +187,32 @@ const headerObserver = new IntersectionObserver(
 headerObserver.observe(header);
 
 
+// revealing elements on scroll
+// revealing the sections
+
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function(entries, observer) {
+    const [entry] = entries;
+    console.log(entry);
+    
+    if (!entry.isIntersecting) return;
+
+    entry.target.classList.remove('section--hidden');
+    observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(
+    revealSection, {
+        root: null,
+        threshold: 0.15
+    });
+
+allSections.forEach(function(section) {
+    sectionObserver.observe(section);
+    section.classList.add('section--hidden');
+});
+
 
 ///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
